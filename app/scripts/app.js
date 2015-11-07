399b427024d36f3fbdf0670ffb2845a86cd42256
stefan512 <veg512@op.pl>
2015-11-07 15:19:19 +0000

    changing about href

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -31,7 +31,7 @@ subject to an additional IP rights grant found at http://polymer.github.io/PATEN
   // See https://github.com/Polymer/polymer/issues/1381
   window.addEventListener('WebComponentsReady', function() {
     // imports are loaded and elements have been registered
-   // app.route = "home";
+    app.route = "home";
   });
 
   // Main area's paper-scroll-header-panel custom condensing transformation of
